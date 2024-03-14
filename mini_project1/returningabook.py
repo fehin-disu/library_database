8a61e41d17becc4ce8ca5e95dbdde6772ab4cb15
samia225 <samia5@ualberta.ca>
2024-03-14 16:21:53 -0600

    Update the logic in returning a book function

--- mini_project1/returningabook.py
+++ mini_project1/returningabook.py
@@ -8,39 +8,49 @@ def returning_a_book(member,path):
     global connection, cursor
     connection, cursor = connect(path)
 
-    returned_book = []
+    returned_book = [] #keeps the track of returned books in a list
     
-    new_book = borrow_book(member)
+    new_book = borrow_book(member) #list of the borrowed books, mainly the borrow ID
     if not new_book:  
         return
 
-    while True:
-            bid = int(input("Enter the Borrowing ID of the book you are returning: "))
+    while True:  
+            bid = int(input("Enter the Borrowing ID of the book you are returning: ")) #If the borrowing ID is correctly typed and within the unreturned book list
             
             if bid in returned_book:
-                print("This book has already been returned. Please enter a different Borrowing ID.")
+                print("This book has already been returned. Please enter a different Borrowing ID.") #checks if the user typed the borrowing ID for a book that has already been returned
                 continue
 
-            if bid in new_book:
-                if return_and_penalty(bid, member):
+            if bid in new_book:   
+                if return_and_penalty(bid, member): #checks if the borrowing ID is the borrowed book list and if its true it tracks the return and calculates the penalty
                     returned_book.append(bid)
 
-                    ask_review = input("Would you like to write a review and add a rating for this book? (yes/no) ")
-                    if ask_review.lower() == "yes":
-                        add_review(bid, member)
-                    
-
-                    if len(returned_book) == len(new_book):
+                    while True:
+                        ask_review = input("Would you like to write a review and add a rating for this book? (yes/no) ")
+                        if ask_review.lower() == "yes":
+                            add_review(bid, member)
+                            break
+                        elif ask_review.lower() != "no":
+                            print("Please select the correct option.")
+                            continue
+                        else:
+                            if ask_review.lower() == "no":
+                                break
+                                
+                    if len(returned_book) == len(new_book): #checks if all the books have been returned
                         print("All books have been returned") 
                         break
-                      
-                    another = input("Do you want to return another book? (yes/no) ")
-                    if another.lower() != "no":
-                        continue
+
+                    another = input("Do you want to return another book? Type yes to return another book and any other character to exit. ")
+                    if another.lower() == "yes":
+                        continue    
+        
                 break
+                              
             else:
                 print("Invalid Borrowing ID. Please try again.")
                 
+    connection.close()
 
     
 
@@ -62,36 +72,41 @@ def borrow_book(member):
         id_values = list()
         for borrowing in borrowings:
             print(f"Borrowing ID: {borrowing[0]}, Title: {borrowing[1]}, Borrowing Date: {borrowing[2]}, Return Deadline: {borrowing[3]}")
-            id_values.append(borrowing[0])
+            id_values.append(borrowing[0]) #keeps a track of the borrowings iD's
     return id_values
 
 
 def return_and_penalty(bid, member):
-    current_date = date.today()  # Get the current date in the correct format
+    current_date = date.today()  # Gets the current date in the correct format
     cursor.execute("""
                     SELECT start_date
                     FROM borrowings
                     WHERE bid = ? AND member = ?
                    """, (bid, member))
-    connection.commit()
+    
     start_date_val = cursor.fetchone()
 
-    if start_date_val is None:
+    if start_date_val is None: #checks if the list is empty
         return False
     
     start_date = datetime.strptime(start_date_val[0], '%Y-%m-%d').date()
     return_deadline = start_date + timedelta(days=20)
 
     overdue_Date = (current_date - return_deadline).days
-    if overdue_Date > 0 and overdue_Date <= 25:
+    if 0< overdue_Date <= 25:
         penalty_amount = overdue_Date
     else:
         if overdue_Date>25:
             penalty_amount = overdue_Date+5
+
+    #insert the penalties update
     cursor.execute("""
                     INSERT INTO penalties (bid, amount)
                     VALUES (?,?)
                    """,(bid,penalty_amount))
+    
+    connection.commit()
+    #update the end date with todays date
     cursor.execute("""UPDATE borrowings 
                    SET end_date = ? WHERE bid = ? AND member = ?
                    """, (current_date, bid, member))
@@ -106,14 +121,14 @@ def add_review(bid, member):
                     WHERE bid = ? AND member = ?
                    """, (bid, member))
     
-    found_book = cursor.fetchone()
+    found_book = cursor.fetchone() #to make sure the book id of the borrowing table is the same as reviews table
     if not found_book:
         return False
     
-    found_book_yes = found_book[0]
+    found_book_yes = found_book[0] #to retrieve the correct book
 
     write_review = input("Please add your review here.\n")
-    #add_rating = int(input("Please rate the book within 1-5: "))
+    
 
     while True:
         add_rating = int(input("Please rate the book within 1-5: "))
@@ -127,8 +142,9 @@ def add_review(bid, member):
         
     review_date= datetime.today()
 
+    #updates the user as a reviewer in the reviews table
     cursor.execute("""
-                    INSERT INTO reviews (book_id, member, rating, rtext, rdate)
+                    INSERT INTO reviews (book_id, member, rating, rtext, rdate) 
                     VALUES (?, ?, ?, ?, ?)
                     """, (found_book_yes, member, add_rating, write_review, review_date))
         
